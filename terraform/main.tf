67b13129c716869a5f2cb795c04f214d57949626
sujeethgowda07-bit <sujeethgowda07@gmail.com>
2026-01-14 14:28:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1"
+  availability_zone       = "ap-south-1a"
 
   tags = {
     Name = "public-subnet"
