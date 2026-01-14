d4d74c8164a9d694bf1bac3e630b2ddd98aa78ae
sujeethgowda07-bit <sujeethgowda07@gmail.com>
2026-01-14 13:52:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
     Name = "ci-cd-vpc"
   }
 }
-#####
+###
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
